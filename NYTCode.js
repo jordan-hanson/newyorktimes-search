69edf803d31562e8ddb9f39af4e4d316de8098c3
Jordan Hanson <JORDANMERCEDES22@GMAIL.COM>
2019-12-19 20:50:30 -0700

    js code with jquery functions and api key connection

--- NYTCode.js
+++ NYTCode.js
@@ -9,6 +9,33 @@ function userSearch() {
 
     })
 
+
 }
+function clear() {
+    $("#article-section").empty();
+}
+
+function buildQueryURL() {
+    var searchTerm = $("#search-term").val().trim();
+    var startYear = $("#start-year").val().trim();
+    var endYear = $("#end-year").val().trim();
+
+    var apiKey = "GvEkVVP76JDuYp26MSr14IAeGQy9XAcB"
+    var nytimesurl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?"
+    var url = nytimesurl + "q=" + searchTerm + "&end_date=" + endYear + "&begin_date=" + startYear + "&api-key=" + apiKey
+
+    console.log(searchTerm);
+    console.log(startYear);
+    console.log(endYear);
+    console.log(url);
+}
+$("#run-search").on("click", function (event) {
+    event.preventDefault();
+    clear();
+    buildQueryURL();
+    // console.log(url)
+})
+
+
 
 userSearch()
